30b9acadfd531877e348bed7dcda8cf90814b45b
Franciel Ubuntu <francielvictor@gmail.com>
2022-10-06 18:36:17 -0300

    primeiros arquivos adicionados

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Descreve Signo</title>
+</head>
+<body>
+    <form action="signos.php" method="POST">
+        <label>Sua data de nascimento.</label>
+        <br>
+        <input type="date" name="data"/>
+        <br>
+        <input type="submit" value="Enviar"/>
+    </form>
+</body>
+</html>
---
+++ signos.php
@@ -0,0 +1,6 @@
+<?php
+    if(isset($_POST['data'])):
+        $data = $_POST['data'];
+        echo var_dump($data);
+    endif;
+?>
