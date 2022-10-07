d9d84ff524544c2b2e5ebdac33fc6d09679b219c
Franciel Ubuntu <francielvictor@gmail.com>
2022-10-06 23:51:30 -0300

    testando strtotime no signos.php

--- signos.php
+++ signos.php
@@ -1,6 +1,8 @@
 <?php
     if(isset($_POST['data'])):
         $data = $_POST['data'];
+        $data = strtotime($data);
+        echo date("d/m", $data)."<br>";
         echo var_dump($data);
     endif;
 ?>
